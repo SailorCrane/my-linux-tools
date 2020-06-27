243ebac56c16667abbe6ab862247b8d54cede15d
Crane <myemail@gmail.com>
2020-06-27 22:05:30 +0800

    modified:   cmdline-settings/common-shell-setting/.common-shell-utils/alias-dir/redis-alias.sh

--- cmdline-settings/common-shell-setting/.common-shell-utils/alias-dir/redis-alias.sh
+++ cmdline-settings/common-shell-setting/.common-shell-utils/alias-dir/redis-alias.sh
@@ -1,6 +1,6 @@
 
-alias rediss="redis-server"
 alias redis="redis-cli"
+alias rediss="redis-server"
 
 alias redis_cluster="redis-cli -p 6379 cluster info"
 alias redis_nodes="redis-cli -p 6379 cluster nodes"
