0411f9fe610abfe955b96004f3afc63e0a2db82a
Crane <myemail@gmail.com>
2020-07-20 19:38:22 +0800

    modified:   ssh-forward.sh

--- cmdline-settings/profile-setting/.myProfile.d/ssh-forward.sh
+++ cmdline-settings/profile-setting/.myProfile.d/ssh-forward.sh
@@ -34,26 +34,26 @@ do_ssh_local_forward() {
     ssh_local_forward   localhost:8080 localhost:8080    crane@ubuntu 7337    # v2ray
     # ssh_local_forward     8081 localhost:8081    crane@ubuntu 7337    # v2ray
 
-    # ssh_local_forward   localhost:6022 10.10.100.26:22   uaq@oldjump  7000   # 预发布 ssh
-    # ssh_local_forward   localhost:6082 10.10.100.26:8082 uaq@oldjump  7000   # flink-web-ui
+    ssh_local_forward   localhost:6022 10.10.100.26:22   uaq@oldjump  7000   # 预发布 ssh
+    ssh_local_forward   localhost:6082 10.10.100.26:8082 uaq@oldjump  7000   # flink-web-ui
 
-    # ssh_local_forward   localhost:2181 10.10.25.50:2181  uaq@oldjump  7000   # flux50 ssh
-    # ssh_local_forward   localhost:5022 10.10.25.50:7777  uaq@oldjump  7000   # flux50 ssh
-    # ssh_local_forward   localhost:5082 10.10.25.50:8082  uaq@oldjump  7000   # flux50 flink
-    # ssh_local_forward   localhost:5083 10.10.25.51:8082  uaq@oldjump  7000   # flux51 flink
+    ssh_local_forward   localhost:2181 10.10.25.50:2181  uaq@oldjump  7000   # flux50 ssh
+    ssh_local_forward   localhost:5022 10.10.25.50:7777  uaq@oldjump  7000   # flux50 ssh
+    ssh_local_forward   localhost:5082 10.10.25.50:8082  uaq@oldjump  7000   # flux50 flink
+    ssh_local_forward   localhost:5083 10.10.25.51:8082  uaq@oldjump  7000   # flux51 flink
 
-    # ssh_local_forward   localhost:2380 10.10.25.230:80   uaq@oldjump  7000   # 230 nginx
+    ssh_local_forward   localhost:2380 10.10.25.230:80   uaq@oldjump  7000   # 230 nginx
 
-    # ssh_local_forward   \*:3389 10.10.25.110:3389  uaq@oldjump 7000   # windows: -u backwin01 -p 123123
+    ssh_local_forward   \*:3389 10.10.25.110:3389  uaq@oldjump 7000   # windows: -u backwin01 -p 123123
 
-    # ssh_local_forward   localhost:3022 10.10.100.13:22   uaq@oldjump  7000 # 13 ssh
-    # ssh_local_forward   localhost:3082 10.10.100.13:8082 uaq@oldjump  7000 # 13 flink
+    ssh_local_forward   localhost:3022 10.10.100.13:22   uaq@oldjump  7000 # 13 ssh
+    ssh_local_forward   localhost:3082 10.10.100.13:8082 uaq@oldjump  7000 # 13 flink
 
-    # ssh_local_forward   localhost:3023 10.10.100.18:22   uaq@oldjump  7000 # 18 ssh
-    # ssh_local_forward   localhost:3083 10.10.100.18:8082 uaq@oldjump  7000 # 18 flink
+    ssh_local_forward   localhost:3023 10.10.100.18:22   uaq@oldjump  7000 # 18 ssh
+    ssh_local_forward   localhost:3083 10.10.100.18:8082 uaq@oldjump  7000 # 18 flink
 
-    # ssh_local_forward   localhost:2022 10.10.25.22:7777  uaq@oldjump  7000 # flux50 ssh
-    # ssh_local_forward   localhost:2082 10.10.25.22:8082  uaq@oldjump  7000 # flux50 flink
+    ssh_local_forward   localhost:2022 10.10.25.22:7777  uaq@oldjump  7000 # flux50 ssh
+    ssh_local_forward   localhost:2082 10.10.25.22:8082  uaq@oldjump  7000 # flux50 flink
 }
 
 
