8f8bbb0e2487ecb56e267181ea663f6d8d308a52
Crane <myemail@gmail.com>
2020-08-17 09:28:30 +0800

    modified:   cmdline-settings/common-shell-setting/.common-shell-utils/alias-dir/keyboard-touchpad-xmod-fusuma-alias.sh add XMOD, XTOG alias (upper case)

--- cmdline-settings/common-shell-setting/.common-shell-utils/alias-dir/keyboard-touchpad-xmod-fusuma-alias.sh
+++ cmdline-settings/common-shell-setting/.common-shell-utils/alias-dir/keyboard-touchpad-xmod-fusuma-alias.sh
@@ -1,7 +1,11 @@
 
-alias   "c2=xmodmap      ~/.Xmodmap"      # ctrl 2 caplock
+alias     c2="xmodmap    ~/.Xmodmap"      # ctrl 2 caplock
+
 alias   xmod="xmodmap    ~/.Xmodmap"      # xmodmap
+alias   XMOD="xmodmap    ~/.Xmodmap"      # xmodmap
+
 alias   xtog="xmodmap    ~/.Xmodtoggle"   # stand for xtoggle
+alias   XTOG="xmodmap    ~/.Xmodtoggle"   # stand for xtoggle
 
 #alias  tpad="nohup sudo fusuma 1>$HOME/.fusuma.nohup &"
 alias  tpad="sudo fusuma"
