246c72cf26a1716924feec57a4bc585e934cb0be
Crane <myemail@gmail.com>
2020-08-27 10:57:32 +0800

    new file:   cmdline-settings/mac-setting/.mac-utils/alias/route-alias.sh

---
+++ cmdline-settings/mac-setting/.mac-utils/alias/route-alias.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# route-alias.sh
+# Copyright (C) 2020 crane <crane@cranedeMacBook-Pro.local>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+alias rtn="env route -n get default"
