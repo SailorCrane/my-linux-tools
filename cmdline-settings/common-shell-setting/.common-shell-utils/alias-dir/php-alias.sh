a1175510fb31a188341e4f4a17318e08fe09bbf6
Crane <myemail@gmail.com>
2020-09-07 10:42:54 +0800

    modified:   cmdline-settings/common-shell-setting/.common-shell-utils/alias-dir/php-alias.sh

--- cmdline-settings/common-shell-setting/.common-shell-utils/alias-dir/php-alias.sh
+++ cmdline-settings/common-shell-setting/.common-shell-utils/alias-dir/php-alias.sh
@@ -1,4 +1,4 @@
 
 # 调整php优先级
-alias php_new="export PATH=/usr/bin:$PATH"
-alias php_old="export PATH=/usr/local/opt/php@7.1/bin:$PATH"
+alias php_new="eval export PATH=/usr/bin:\$PATH"
+alias php_old="eval export PATH=/usr/local/opt/php@7.1/bin:\$PATH"
