0a90ebc1dde66a274d0e872d7c5006319b2a40a9
Crane <myemail@gmail.com>
2020-09-22 16:20:43 +0800

    modified:   cmdline-settings/mac-setting/.mac-utils/alias/brew-service-alias.sh

--- cmdline-settings/mac-setting/.mac-utils/alias/brew-service-alias.sh
+++ cmdline-settings/mac-setting/.mac-utils/alias/brew-service-alias.sh
@@ -2,4 +2,9 @@
 alias srst="brew services restart"
 alias sst="brew services start"
 alias sstp="brew services stop"
-alias sss="brew services restart"
+
+unalias sss
+sss() {
+    local Service=$1
+    brew services list | grep $Service -C 5
+}
