9fae9bd89a87c2333b24306bb9fd89eed81e0606
Crane <myemail@gmail.com>
2020-09-24 17:10:57 +0800

    new file:   cmdline-settings/mac-setting/.mac-utils/alias/golang-alias.sh

---
+++ cmdline-settings/mac-setting/.mac-utils/alias/golang-alias.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# golang-alias.sh
+# Copyright (C) 2020 crane <crane@cranedeMacBook-Pro.local>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+alias goland="open -a Goland"
