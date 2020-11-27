69825dbebc430ef31159c154b204833b37e1ca9b
Crane <SailorCrane@qq.com>
2020-11-26 21:39:44 -0600

    new file:   cmdline-settings/mac-setting/.mac-utils/alias/common-alias.sh

---
+++ cmdline-settings/mac-setting/.mac-utils/alias/common-alias.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+#
+# common-alias.sh
+# Copyright (C) 2020 crane <crane@cranetekiMacBookPro.local>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+alias timezone="sudo systemsetup -gettimezone"
+alias tz="sudo systemsetup -gettimezone"
