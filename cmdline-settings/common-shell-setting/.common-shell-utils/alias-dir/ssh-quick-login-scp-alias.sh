a7c8a73718fa4f63777c7a7204f2bd5ee873be80
Crane <SailorCrane@qq.com>
2021-02-27 11:03:10 +0800

    modified:   cmdline-settings/common-shell-setting/.common-shell-utils/alias-dir/ssh-quick-login-scp-alias.sh added quick_pull_partial

--- cmdline-settings/common-shell-setting/.common-shell-utils/alias-dir/ssh-quick-login-scp-alias.sh
+++ cmdline-settings/common-shell-setting/.common-shell-utils/alias-dir/ssh-quick-login-scp-alias.sh
@@ -37,6 +37,22 @@ quick_push() {
     scp -r -P $Port $LocalPath $Url:$RemotePath
 }
 
+quick_push_partial() {
+    Url=$1
+    Port=$2
+    # shift 2
+    LocalPath=$3     # 可以是文件或者目录
+
+    RemotePath="/tmp"
+    if [[ $# == 4 ]] ;then
+        RemotePath=$4
+    fi
+    echo "remotePath is $RemotePath"
+
+    rsync -rP --rsh="ssh -p$Port" $LocalPath $Url:$RemotePath
+}
+
+
 quick_pull() {
     Url=$1
     Port=$2
@@ -56,6 +72,27 @@ quick_pull() {
 }
 
 
+# 断电续传(通过rsync)
+quick_pull_partial() {
+    Url=$1
+    Port=$2
+    # shift 2
+    RemotePath=$3     # 可以是文件或者目录
+
+    #LocalPath="/tmp"
+    LocalPath="./"
+    if [[ $# == 4 ]] ;then
+        LocalPath=$4
+        # echo "$#"
+    fi
+    echo "localPath is $LocalPath"
+
+    rsync -rP --rsh="ssh -p$Port" $Url:$RemotePath  $LocalPath
+    # scp -r -P $Port $Url:$RemotePath $LocalPath
+    echo "Pull remote file [${RemotePath}] into local path [${LocalPath}], "--partial" option"
+}
+
+
 alias ubuntup="quick_push crane@ubuntu 7337"
 alias tapp="quick_push crane@tap 22"
 # alias 50p="quick_push crane@122.228.212.146 7777"
@@ -70,7 +107,10 @@ alias tapp="quick_push crane@tap 22"
 
 
 alias ubuntul="quick_pull crane@ubuntu 7337"
-alias tapl="quick_pull crane@tap 22"
+# alias tapl="quick_pull crane@tap 22"
+
+alias tapp="quick_push_partial crane@tap 22"
+alias tapl="quick_pull_partial crane@tap 22"
 # alias 50l="quick_pull $Ssh50Host $Ssh50Port"
 # alias 22l="quick_pull root@localhost 2022"
 # alias oldl="quick_pull uaq@101.71.28.84 7000"
